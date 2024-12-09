85fdcd11977fa34b3b573ad4705f94356fb90a19
Barry Richards <barryrichards05@gmail.com>
2024-12-09 18:15:48 +0000

    added comment about creating functions

--- Project.js
+++ Project.js
@@ -1,12 +1,15 @@
 const goodiesList = ['The Doctor', 'Rose Tyler', 'Donna Noble', 'Mickey Smith', 'Sarah Jane Smith',
     'K-9', 'Martha Jones', 'River Song', 'Amy Pond', 'Rory Williams', 'Wilfred Mott',
-    'Captain Jack Harkness', 'Jackie Tyler', 'Kate Stewart', 'Clara Oswald'];
+    'Captain Jack Harkness', 'Jackie Tyler', 'Kate Stewart', 'Clara Oswald', 'The Face of Boe'];
 
 const baddiesList = ['a Dalek', 'a Cyberman', 'a Sontaran', 'The Master', 'Zygons',
-    'Vashta Narada', 'a Weeping Angel', 'The Silence', 'The Family of Blood', 'The Midnight Creature'
+    'Vashta Narada', 'a Weeping Angel', 'The Silence', 'The Family of Blood', 'The Midnight Creature',
     'The Beast', 'The Abzorbaloff', 'The Adipose', 'Lady Cassandra', 'Maestro',
     'The Celestial Toymaker', 'Pig Slaves', 'The Empty Child', 'The Slitheen'];
 
 const locationsList = ['Earth', 'The Powell Estate', 'Gallifrey', 'Clom',
     'Raxacoricofallapatorius', 'New Earth', 'Adipose 3', `Demon's Run`, 'Skaro', 'Krop Tor',
-    'The Library', 'Midnight', 'Sontar', 'Trenzalore', '']
+    'The Library', 'Midnight', 'Sontar', 'Trenzalore', 'Platform One'];  
+
+// // Create functions to select random elements from each array, assign those elements to
+// new variables and print a statement to console
