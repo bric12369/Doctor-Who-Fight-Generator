619b80472bb931a024cd0eeb4f466e484937dd25
Barry Richards <barryrichards05@gmail.com>
2024-12-09 18:02:47 +0000

    first commit

---
+++ Project.js
@@ -0,0 +1,12 @@
+const goodiesList = ['The Doctor', 'Rose Tyler', 'Donna Noble', 'Mickey Smith', 'Sarah Jane Smith',
+    'K-9', 'Martha Jones', 'River Song', 'Amy Pond', 'Rory Williams', 'Wilfred Mott',
+    'Captain Jack Harkness', 'Jackie Tyler', 'Kate Stewart', 'Clara Oswald'];
+
+const baddiesList = ['a Dalek', 'a Cyberman', 'a Sontaran', 'The Master', 'Zygons',
+    'Vashta Narada', 'a Weeping Angel', 'The Silence', 'The Family of Blood', 'The Midnight Creature'
+    'The Beast', 'The Abzorbaloff', 'The Adipose', 'Lady Cassandra', 'Maestro',
+    'The Celestial Toymaker', 'Pig Slaves', 'The Empty Child', 'The Slitheen'];
+
+const locationsList = ['Earth', 'The Powell Estate', 'Gallifrey', 'Clom',
+    'Raxacoricofallapatorius', 'New Earth', 'Adipose 3', `Demon's Run`, 'Skaro', 'Krop Tor',
+    'The Library', 'Midnight', 'Sontar', 'Trenzalore', '']
